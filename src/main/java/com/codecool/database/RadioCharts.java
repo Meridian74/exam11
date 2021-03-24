7e7867baf0b1e860d918ea34e77cff9756bdb3fa
meridian74 <meridian@freemail.hu>
2021-03-24 16:49:22 +0100

    almost done... 2 test remained.

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,57 @@
 package com.codecool.database;
 
 
+import java.sql.*;
+
 public class RadioCharts {
-    //TODO: implement this class
+    private final String DB_URL;
+    private final String DB_USER;
+    private final String DB_PASSWORD;
+
+    public RadioCharts(String db_url, String db_user, String db_password) {
+        DB_URL = db_url;
+        DB_USER = db_user;
+        DB_PASSWORD = db_password;
+    }
+
+    public String getMostPlayedSong() {
+        String result = "";
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            String sql = "SELECT song, SUM(times_aired) AS aired\n"
+                    + "FROM music_broadcast\n"
+                    + "GROUP BY song\n"
+                    + "ORDER BY aired DESC LIMIT 1";
+
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                result = rs.getString("song");
+            }
+            rs.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return result;
+    }
+
+    public String getMostActiveArtist() {
+        String result = "";
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            String sql = "SELECT artist, COUNT(song) AS countered\n"
+                    + "FROM music_broadcast\n"
+                    + "GROUP BY artist\n"
+                    + "ORDER BY countered DESC LIMIT 1";
+
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                result = rs.getString("artist");
+            }
+            rs.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return result;
+    }
+
 }
